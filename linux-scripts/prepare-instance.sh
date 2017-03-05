5edfafb252f0a98dacf3992792c7be058e8069b5
Luis Yucra <luis.yucra@gmail.com>
2017-03-05 00:25:59 -0500

    added a script the install a base linux env

---
+++ linux-scripts/prepare-instance.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+./create-nvme-fs.sh
+./install-apache.sh
+./install-java.sh
+./install-ant.sh
+./install-maven.sh
+./install-nodejs.sh
+./install-mysql-server.sh
+./install-php.sh
+
