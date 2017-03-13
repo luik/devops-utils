256348b50aeaab2562634e48d2f69bbc15ac24dd
alvaro <alvarolinarez9@gmail.com>
2017-03-13 11:23:27 -0500

    Add Exchange Installation Features

--- windows-scripts/start-exchange2013-installation.ps1
+++ windows-scripts/start-exchange2013-installation.ps1
@@ -2,4 +2,7 @@ mkdir D:\tmp\
 (new-object System.Net.WebClient).DownloadFile('https://download.microsoft.com/download/2/C/4/2C47A5C1-A1F3-4843-B9FE-84C0032C61EC/UcmaRuntimeSetup.exe','D:\tmp\UcmaRuntimeSetup.exe')
 D:\tmp\UcmaRuntimeSetup.exe
 
+Add-WindowsFeature Server-Media-Foundation
+
+
 
