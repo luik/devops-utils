7f2bdcc9aa96a437c7d65eb7c03971306203dd1b
Luis Yucra Ccolqque <luis.yucra@gmail.com>
2018-04-29 12:38:32 -0500

    Update install-docker-compose.sh

--- linux-scripts/install-docker-compose.sh
+++ linux-scripts/install-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
