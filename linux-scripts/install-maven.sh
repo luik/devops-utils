ce6cd13cc5b21ea82e8c53582837c18a484557ca
Luis Yucra Ccolqque <luis.yucra@gmail.com>
2019-04-03 08:57:36 -0500

    Update install-maven.sh

--- linux-scripts/install-maven.sh
+++ linux-scripts/install-maven.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MAVEN_VERSION=3.5.0
+MAVEN_VERSION=3.6.0
 
 cd /tmp
 curl -O -L "http://www-eu.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"
