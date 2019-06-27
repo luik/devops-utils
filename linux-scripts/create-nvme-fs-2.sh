ead26c5d24d61fa7c0a16835b7d92188a569c58c
Luis Yucra Ccolqque <luis.yucra@gmail.com>
2019-06-27 06:16:15 -0500

    Create create-nvme-fs-2.sh

---
+++ linux-scripts/create-nvme-fs-2.sh
@@ -0,0 +1,22 @@
+ #!/usr/bin/env bash
+
+sudo fdisk /dev/nvme1n1 <<EOF
+n
+p
+1
+
+
+w
+EOF
+
+sleep 3
+
+sudo mkfs.ext4 /dev/nvme1n1p1 <<EOF
+y
+EOF
+
+sudo mkdir /media/nvme1
+sudo mount /dev/nvme0n1p1 /media/nvme1
+sudo mkdir /media/nvme1/data
+sudo chown ubuntu:ubuntu /media/nvme1/data
+
