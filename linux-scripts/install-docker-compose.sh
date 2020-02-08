1b42bbae29f70ff6f50d271c85439f45326d59c6
Luis Yucra Ccolqque <luis.yucra@gmail.com>
2020-02-08 11:55:24 -0500

    Update install-docker-compose.sh

--- linux-scripts/install-docker-compose.sh
+++ linux-scripts/install-docker-compose.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#Ref https://github.com/docker/compose/releases
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
