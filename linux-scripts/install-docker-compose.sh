e720668cfd26228eed944840e33cee751c875407
Luis Alberto Yucra Ccolqque <luis.yucra@gmail.com>
2021-09-06 12:41:32 -0500

    Update install-docker-compose.sh
    update composer version

--- linux-scripts/install-docker-compose.sh
+++ linux-scripts/install-docker-compose.sh
@@ -2,6 +2,6 @@
 
 #Ref https://github.com/docker/compose/releases
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
